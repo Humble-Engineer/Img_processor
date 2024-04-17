23744b9c020decf793cfce62b02a406397d9c7df
Windows Asus <1600767031@qq.com>
2024-04-17 10:32:07 +0800

    添加色彩空间下拉选项框

--- MainWindow_ui.py
+++ MainWindow_ui.py
@@ -15,9 +15,9 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QFrame, QLabel, QMainWindow,
-    QMenuBar, QPushButton, QSizePolicy, QStatusBar,
-    QWidget)
+from PySide6.QtWidgets import (QApplication, QComboBox, QFrame, QLabel,
+    QMainWindow, QMenuBar, QPushButton, QSizePolicy,
+    QStatusBar, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -34,10 +34,10 @@ class Ui_MainWindow(object):
         self.origin_img.setObjectName(u"origin_img")
         self.origin_img.setGeometry(QRect(10, 30, 330, 330))
         self.origin_img.setFrameShape(QFrame.Shape.Panel)
-        self.processed_img = QLabel(self.centralwidget)
-        self.processed_img.setObjectName(u"processed_img")
-        self.processed_img.setGeometry(QRect(360, 30, 330, 330))
-        self.processed_img.setFrameShape(QFrame.Shape.Panel)
+        self.result_img = QLabel(self.centralwidget)
+        self.result_img.setObjectName(u"result_img")
+        self.result_img.setGeometry(QRect(360, 30, 330, 330))
+        self.result_img.setFrameShape(QFrame.Shape.Panel)
         self.save_button = QPushButton(self.centralwidget)
         self.save_button.setObjectName(u"save_button")
         self.save_button.setGeometry(QRect(10, 440, 100, 80))
@@ -52,12 +52,6 @@ class Ui_MainWindow(object):
         self.load_button.setEnabled(True)
         self.load_button.setGeometry(QRect(10, 360, 100, 80))
         self.load_button.setFont(font1)
-        self.rgb2gray_button = QPushButton(self.centralwidget)
-        self.rgb2gray_button.setObjectName(u"rgb2gray_button")
-        self.rgb2gray_button.setGeometry(QRect(110, 360, 100, 80))
-        self.rgb2hsv_button = QPushButton(self.centralwidget)
-        self.rgb2hsv_button.setObjectName(u"rgb2hsv_button")
-        self.rgb2hsv_button.setGeometry(QRect(110, 440, 100, 80))
         self.label = QLabel(self.centralwidget)
         self.label.setObjectName(u"label")
         self.label.setGeometry(QRect(150, 0, 71, 31))
@@ -69,6 +63,19 @@ class Ui_MainWindow(object):
         self.label_2.setObjectName(u"label_2")
         self.label_2.setGeometry(QRect(500, 0, 81, 31))
         self.label_2.setFont(font2)
+        self.color_space_Box = QComboBox(self.centralwidget)
+        self.color_space_Box.addItem("")
+        self.color_space_Box.addItem("")
+        self.color_space_Box.addItem("")
+        self.color_space_Box.setObjectName(u"color_space_Box")
+        self.color_space_Box.setGeometry(QRect(110, 385, 100, 55))
+        self.change_color_space_button = QPushButton(self.centralwidget)
+        self.change_color_space_button.setObjectName(u"change_color_space_button")
+        self.change_color_space_button.setGeometry(QRect(110, 440, 100, 80))
+        self.label_3 = QLabel(self.centralwidget)
+        self.label_3.setObjectName(u"label_3")
+        self.label_3.setGeometry(QRect(110, 360, 101, 31))
+        self.label_3.setAlignment(Qt.AlignmentFlag.AlignCenter)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
@@ -86,12 +93,16 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
         self.origin_img.setText("")
-        self.processed_img.setText("")
-        self.save_button.setText(QCoreApplication.translate("MainWindow", u"\u4fdd\u5b58\u56fe\u7247", None))
-        self.load_button.setText(QCoreApplication.translate("MainWindow", u"\u52a0\u8f7d\u56fe\u7247", None))
-        self.rgb2gray_button.setText(QCoreApplication.translate("MainWindow", u"\u8f6c\u4e3a\u7070\u5ea6\u56fe\u50cf", None))
-        self.rgb2hsv_button.setText(QCoreApplication.translate("MainWindow", u"\u8f6c\u5230HSV\u7a7a\u95f4", None))
+        self.result_img.setText("")
+        self.save_button.setText(QCoreApplication.translate("MainWindow", u"\u4fdd\u5b58\u56fe\u50cf", None))
+        self.load_button.setText(QCoreApplication.translate("MainWindow", u"\u9009\u62e9\u56fe\u50cf", None))
         self.label.setText(QCoreApplication.translate("MainWindow", u"\u5904\u7406\u524d\u56fe\u50cf", None))
         self.label_2.setText(QCoreApplication.translate("MainWindow", u"\u5904\u7406\u540e\u56fe\u50cf", None))
+        self.color_space_Box.setItemText(0, QCoreApplication.translate("MainWindow", u"GRAY", None))
+        self.color_space_Box.setItemText(1, QCoreApplication.translate("MainWindow", u"HSV", None))
+        self.color_space_Box.setItemText(2, QCoreApplication.translate("MainWindow", u"YCrCb", None))
+
+        self.change_color_space_button.setText(QCoreApplication.translate("MainWindow", u"\u8f6c\u6362\u8272\u5f69\u7a7a\u95f4", None))
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u"\u76ee\u6807\u8272\u5f69\u7a7a\u95f4", None))
     # retranslateUi
 
--- main.py
+++ main.py
@@ -23,6 +23,13 @@ class MainWindow(QMainWindow):
 
         self.band()  # 调用band方法进行进一步的初始化或设置
 
+        # 默认预加载的图像
+        self.origin_img = cv2.imread("materials\cartoon.png")
+        # 获取图像的维度信息
+        self.height, self.width, self.channels = self.origin_img.shape
+        # 显示预加载的图像
+        self.display_origin_image()
+
     def band(self):
         """
         绑定按钮或菜单项的点击事件到相应的函数。
@@ -38,10 +45,10 @@ class MainWindow(QMainWindow):
         self.ui.load_button.clicked.connect(self.load_image)
         # 绑定保存图像按钮的点击事件
         self.ui.save_button.clicked.connect(self.save_image)
-        # 绑定灰度化按钮的点击事件
-        self.ui.rgb2gray_button.clicked.connect(self.RGB2GRAY)
-        # 绑定HSV按钮的点击事件
-        self.ui.rgb2hsv_button.clicked.connect(self.RGB2HSV)
+
+        # 绑定图像颜色空间转换按钮的点击事件
+        self.ui.change_color_space_button.clicked.connect(self.change_color_space)
+
 
     def load_image(self):
         """
@@ -54,15 +61,21 @@ class MainWindow(QMainWindow):
 
         # 如果用户选择了文件，则加载并显示图像
         if selected_file:
+            # 将路径转换为Path对象
             image_path = Path(selected_file)
-            self.display_origin_image(image_path)
+            # 使用 OpenCV 读取图像，并保存为属性
+            self.origin_img = cv2.imread(str(image_path))
+            # 获取图像的维度信息
+            self.height, self.width, self.channels = self.origin_img.shape
+            # 显示原始图像
+            self.display_origin_image()
 
     def save_image(self):
         """
         保存当前处理完成后的图像。
         """
         # 获取当前显示的QPixmap
-        current_pixmap = self.ui.processed_img.pixmap()
+        current_pixmap = self.ui.result_img.pixmap()
 
         if current_pixmap.isNull():
             print("No image to save.")
@@ -81,13 +94,11 @@ class MainWindow(QMainWindow):
         if not current_pixmap.save(save_path, "PNG"):
             print(f"Failed to save image to {save_path}")
 
-    def display_origin_image(self, image_path):
+    def display_origin_image(self):
         """
         显示原始图像
         """
-        # 使用 OpenCV 读取图像，并保存为属性
-        self.origin_img = cv2.imread(str(image_path))
-
+        
         if self.origin_img is not None:
             # 将图像从 BGR 转换为 RGB 格式以适应 Qt
             img_rgb = cv2.cvtColor(self.origin_img, cv2.COLOR_BGR2RGB)
@@ -109,15 +120,16 @@ class MainWindow(QMainWindow):
             self.ui.origin_img.setPixmap(scaled_pixmap)
         else:
             # 如果图像加载失败，打印错误信息
-            print(f"Failed to load image from {image_path}")
+            print(f"Failed to display origin image!")
 
-    def display_processed_image(self):
+    def display_result_image(self):
         """
         显示处理后的图像
         """
-        if self.processed_img is not None:
+        if self.result_img is not None:
+
             # 将图像从 BGR 转换为 RGB 格式以适应 Qt
-            img_rgb = cv2.cvtColor(self.processed_img, cv2.COLOR_BGR2RGB)
+            img_rgb = cv2.cvtColor(self.result_img, cv2.COLOR_BGR2RGB)
 
             # 创建 QImage 并使用图像数据初始化
             qimg = QImage(img_rgb.data, img_rgb.shape[1], img_rgb.shape[0], QImage.Format_RGB888)
@@ -126,35 +138,33 @@ class MainWindow(QMainWindow):
             original_pixmap = QPixmap.fromImage(qimg)
 
             # 获取 QLabel 的尺寸
-            label_width = self.ui.processed_img.width()
-            label_height = self.ui.processed_img.height()
+            label_width = self.ui.result_img.width()
+            label_height = self.ui.result_img.height()
 
             # 将 QPixmap 缩放到 QLabel 大小，保持原图长宽比
             scaled_pixmap = original_pixmap.scaled(label_width, label_height)
 
             # 将缩放后的 QPixmap 设置为 QLabel 的内容
-            self.ui.processed_img.setPixmap(scaled_pixmap)
+            self.ui.result_img.setPixmap(scaled_pixmap)
         else:
             # 如果图像加载失败，打印错误信息
-            print(f"Failed to show processed image")
+            print(f"Failed to display result image!")
 
-    def RGB2GRAY(self):
-        """
-        将图像转换为灰度图像
-        """
-        # 使用 OpenCV 将图像转换为灰度图像，并保存为属性
-        self.processed_img = cv2.cvtColor(self.origin_img, cv2.COLOR_BGR2GRAY)
-        self.display_processed_image()
+    def change_color_space(self):
 
-    def RGB2HSV(self):
-        """
-        将图像转换为HSV图像
-        """
-        # 使用 OpenCV 将图像转换为HSV图像，并保存为属性
-        self.processed_img = cv2.cvtColor(self.origin_img, cv2.COLOR_BGR2HSV)
-        self.display_processed_image()
+        if (self.ui.color_space_Box.currentText() == "HSV"):
+            self.result_img = cv2.cvtColor(self.origin_img, cv2.COLOR_BGR2HSV)
+        
+        elif (self.ui.color_space_Box.currentText() == "GRAY"):
+            self.result_img = cv2.cvtColor(self.origin_img, cv2.COLOR_BGR2GRAY)
 
+        elif (self.ui.color_space_Box.currentText() == "YCrCb"):
+            self.result_img = cv2.cvtColor(self.origin_img, cv2.COLOR_BGR2YCrCb)
 
+        else :
+            self.result_img = self.origin_img
+
+        self.display_result_image()
 
 if __name__ == "__main__":
     # 创建 QApplication 实例
